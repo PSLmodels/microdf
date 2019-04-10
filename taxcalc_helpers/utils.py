404ad9c1fe0d6cafaab567fc88b6a34d6eac2e6c
Max Ghenis <mghenis@gmail.com>
2019-04-10 16:42:31 -0700

    Add weighted_quantile function

--- taxcalc_helpers/utils.py
+++ taxcalc_helpers/utils.py
@@ -246,3 +246,43 @@ def cash_income(df):
             (1 - TANF_CASH_SHARE) * df.tanf_ben -
             (1 - VET_CASH_SHARE) * df.vet_ben -
             (1 - WIC_CASH_SHARE) * df.wic_ben)
+
+def weighted_quantile(values, quantiles, sample_weight=None, 
+                      values_sorted=False, old_style=False):
+    """ Very close to numpy.percentile, but supports weights.
+    
+    From https://stackoverflow.com/a/29677616/1840471.
+    
+    Args:
+        values: numpy array with data.
+        quantiles: array-like with many quantiles needed ([0, 1]).
+        sample_weight: array-like of the same length as `array`.
+        values_sorted: bool, if True, then will avoid sorting of
+            initial array
+        old_style: if True, will correct output to be consistent
+            with numpy.percentile.
+    
+    Returns:
+        numpy.array with computed quantiles.
+    """
+    values = np.array(values)
+    quantiles = np.array(quantiles)
+    if sample_weight is None:
+        sample_weight = np.ones(len(values))
+    sample_weight = np.array(sample_weight)
+    assert np.all(quantiles >= 0) and np.all(quantiles <= 1), \
+        'quantiles should be in [0, 1]'
+
+    if not values_sorted:
+        sorter = np.argsort(values)
+        values = values[sorter]
+        sample_weight = sample_weight[sorter]
+
+    weighted_quantiles = np.cumsum(sample_weight) - 0.5 * sample_weight
+    if old_style:
+        # To be convenient with numpy.percentile
+        weighted_quantiles -= weighted_quantiles[0]
+        weighted_quantiles /= weighted_quantiles[-1]
+    else:
+        weighted_quantiles /= np.sum(sample_weight)
+    return np.interp(quantiles, weighted_quantiles, values)
