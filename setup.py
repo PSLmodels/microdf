e42634bd7052fb24d72d71d526026f246c884421
Max Ghenis <mghenis@gmail.com>
2019-04-10 19:10:34 -0700

    Add note about labellines import.

--- setup.py
+++ setup.py
@@ -14,6 +14,6 @@ setup(name='taxcalc_helpers',
           'taxcalc',
           'matplotlib',
           'seaborn',
-          'labellines'
+          # 'labellines'  # Causing an error, but works without.
       ],
       zip_safe=False)
