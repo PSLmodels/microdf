9481de51abc8d59ef90ca3155d4084299f7e5c6a
Max Ghenis <mghenis@gmail.com>
2019-05-03 22:09:49 -0700

    Create functions based on add_custom_tax for VAT, FTT, and carbon tax.

--- taxcalc_helpers/custom_taxes.py
+++ taxcalc_helpers/custom_taxes.py
@@ -5,18 +5,23 @@ import numpy as np
 import pandas as pd
 import taxcalc_helpers as tch
 
-# TODO: Make these separate and then create a function to merge them.
-INCIDENCE = pd.DataFrame({
-    'income_percentile_floor': [-1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 95, 99, 99.9],
-    # Source: https://www.taxpolicycenter.org/briefing-book/who-would-bear-burden-vat
-    'vat': [3.9, 3.9, 3.6, 3.6, 3.6, 3.6, 3.6, 3.4, 3.4, 3.2, 2.8, 2.5, 2.5],
-    # Source: Table 5 in https://www.treasury.gov/resource-center/tax-policy/tax-analysis/Documents/WP-115.pdf
-    'carbon_tax': [0.8, 1.2, 1.4, 1.5, 1.6, 1.7, 1.8, 1.8, 1.8, 1.8, 1.6, 1.4, 0.7],
-    # Source: Figure 1 in https://www.taxpolicycenter.org/sites/default/files/alfresco/publication-pdfs/2000587-financial-transaction-taxes.pdf
-    'ftt': [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.2, 0.2, 0.2, 0.3, 0.4, 0.8, 1.0]
-})
+# Source: https://www.taxpolicycenter.org/briefing-book/who-would-bear-burden-vat
+VAT_INCIDENCE = pd.Series(
+        index=[-1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 95, 99, 99.9],
+        data=[3.9, 3.9, 3.6, 3.6, 3.6, 3.6, 3.6, 3.4, 3.4, 3.2, 2.8, 2.5, 2.5])
+VAT_INCIDENCE /= 100
 
-INCIDENCE[['vat', 'carbon_tax', 'ftt']] /= 100
+# Source: Table 5 in https://www.treasury.gov/resource-center/tax-policy/tax-analysis/Documents/WP-115.pdf
+CARBON_TAX_INCIDENCE = pd.Series(
+        index=[-1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 95, 99, 99.9],
+        data=[0.8, 1.2, 1.4, 1.5, 1.6, 1.7, 1.8, 1.8, 1.8, 1.8, 1.6, 1.4, 0.7])
+CARBON_TAX_INCIDENCE /= 100
+
+# Source: Figure 1 in https://www.taxpolicycenter.org/sites/default/files/alfresco/publication-pdfs/2000587-financial-transaction-taxes.pdf
+FTT_INCIDENCE = pd.Series(
+        index=[-1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 95, 99, 99.9],
+        data=[0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.2, 0.2, 0.2, 0.3, 0.4, 0.8, 1.0])
+FTT_INCIDENCE /= 100
 
 
 def add_custom_tax(df, segment_income, w, base_income, incidence, prefix):
@@ -34,38 +39,61 @@ def add_custom_tax(df, segment_income, w, base_income, incidence, prefix):
 
     Returns:
         Nothing. Adds two columns to df:
-        * [prefix]_tax_incidence: Rate.
-        * [prefix]_tax_liability: Rate * base_income.
+        * [prefix]_incidence: Rate.
+        * [prefix]_liability: Rate * base_income.
         df is also sorted by segment_income.
     """
     df.sort_values(segment_income, inplace=True)
     income_percentile = 100 * df[w].cumsum() / df[w].sum()
-    df[prefix + '_tax_incidence'] = incidence.iloc[
+    df[prefix + 'incidence'] = incidence.iloc[
         pd.cut(income_percentile,
                # Add a right endpoint. Should be 100 but sometimes a decimal gets added.
                bins=incidence.index.tolist() + [101],
                labels=False)].values
-    df[prefix + '_tax_liability'] = df[prefix + '_tax_incidence'] * df[base_income]
+    df[prefix + '_liability'] = df[prefix + '_incidence'] * df[base_income]
     
 
-def add_carbon_tax(df, incidence=INCIDENCE):
-    """Estimate carbon tax liability.
-
+def add_vat(df):
+    """Add value added tax based on incidence estimate from Tax Policy Center.
+    
     Args:
-        df: DataFrame with tpc_eci and XTOT_m columns. Use TPC methodology to
-            align with their incidence estimate.
-        incidence: DataFrame with income_percentile_floor and carbon_tax columns.
+        df: DataFrame with columns for tpc_eci, XTOT_m, and aftertax_income.
 
     Returns:
-        Nothing. Two columns are added:
-        * carbon_tax_incidence: Share of income.
-        * carbon_tax_liability: Liability (carbon_tax_incidence * aftertax_income).
-        It also sorts df by tpc_eci as part of the calculation.
+        Nothing. Adds two columns to df:
+        * vat_incidence: Rate.
+        * vat_liability: Rate * aftertax_income.
+        df is also sorted by tpc_eci.
     """
-    df.sort_values('tpc_eci', inplace=True)
-    income_percentile = df.XTOT_m.cumsum() / df.XTOT_m.sum()
-    df['carbon_tax_incidence'] = INCIDENCE.vat[
-        pd.cut(income_percentile,
-               bins=INCIDENCE.income_percentile_floor.tolist() + [100],
-               labels=False)].values
-    df['carbon_tax_liability'] = df.carbon_tax_incidence * df.aftertax_income
+    add_custom_tax(df, 'tpc_eci', 'XTOT_m', 'aftertax_income', VAT_INCIDENCE, 'vat')
+
+
+def add_carbon_tax(df):
+    """Add carbon tax based on incidence estimate from the US Treasury Department.
+    
+    Args:
+        df: DataFrame with columns for tpc_eci, XTOT_m, and aftertax_income.
+
+    Returns:
+        Nothing. Adds two columns to df:
+        * carbon_tax_incidence: Rate.
+        * carbon_tax_liability: Rate * aftertax_income.
+        df is also sorted by tpc_eci.
+    """
+    add_custom_tax(df, 'tpc_eci', 'XTOT_m', 'aftertax_income',
+                   CARBON_TAX_INCIDENCE, 'carbon_tax')
+
+    
+def add_ftt(df):
+    """Add financial transaction tax based on incidence estimate from Tax Policy Center.
+    
+    Args:
+        df: DataFrame with columns for tpc_eci, XTOT_m, and aftertax_income.
+
+    Returns:
+        Nothing. Adds two columns to df:
+        * ftt_incidence: Rate.
+        * ftt_liability: Rate * aftertax_income.
+        df is also sorted by tpc_eci.
+    """
+    add_custom_tax(df, 'tpc_eci', 'XTOT_m', 'aftertax_income', FTT_INCIDENCE, 'ftt')
