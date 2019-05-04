aaf752e56acbf6d5763a17d08bf2357fcd474c21
Max Ghenis <mghenis@gmail.com>
2019-05-03 20:46:14 -0700

    Add tpc_eci function.

--- taxcalc_helpers/income_measures.py
+++ taxcalc_helpers/income_measures.py
@@ -27,3 +27,21 @@ def cash_income(df):
             (1 - tc.TANF_CASH_SHARE) * df.tanf_ben -
             (1 - tc.VET_CASH_SHARE) * df.vet_ben -
             (1 - tc.WIC_CASH_SHARE) * df.wic_ben)
+
+
+def tpc_eci(df):
+    """Approximates Tax Policy Center's Expanded Cash Income measure.
+
+    Subtracts WIC, housing assistance, veteran's benefits, Medicare, and
+    Medicaid from expanded_income. ECI adds income measures not modeled in
+    Tax-Calculator, so these are ignored and will create a discrepancy
+    compared to TPC's ECI.
+   
+    Args:
+        df: DataFrame with columns from Tax-Calculator.
+
+    Returns:
+        pandas Series with TPC's ECI.
+    """
+    ECI_REMOVE_COLS = ['wic_ben', 'housing_ben', 'vet_ben', 'mcare_ben', 'mcaid_ben']
+    return df.expanded_income - df[ECI_REMOVE_COLS].sum(axis=1)
