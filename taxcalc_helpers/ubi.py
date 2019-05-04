ed5556118e1a878af365cec82b55c6f451257829
Max Ghenis <mghenis@gmail.com>
2019-05-04 04:12:37 -0700

    Fix bug in ubi_or_bens.

--- taxcalc_helpers/ubi.py
+++ taxcalc_helpers/ubi.py
@@ -19,11 +19,9 @@ def ubi_or_bens(df, ben_cols, max_ubi='max_ubi', ubi='ubi', bens='bens'):
         Nothing. Benefits in ben_cols are adjusted, and ubi and bens columns
         are added.
     """
-    bens = df[ben_cols].sum(axis=1)
-    take_ubi = base[max_ubi] > base.bens
-    df[ubi] = np.where(take_ubi, base[max_ubi], 0)
-    df[ben_cols] *= np.where(take_ubi, 0, 1)
+    total_bens = df[ben_cols].sum(axis=1)
+    take_ubi = df[max_ubi] > total_bens
+    df[ubi] = np.where(take_ubi, df[max_ubi], 0)
+    for ben in ben_cols:
+        df[ben] *= np.where(take_ubi, 0, 1)
     df[bens] = df[ben_cols].sum(axis=1)
-
-    
-    
