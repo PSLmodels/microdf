a465b81f7c5affe036290b23576d5156dfe65f59
Max Ghenis <mghenis@gmail.com>
2019-05-03 21:40:27 -0700

    Add generic function for estimating liability of a custom tax.

--- taxcalc_helpers/custom_taxes.py
+++ taxcalc_helpers/custom_taxes.py
@@ -15,3 +15,56 @@ INCIDENCE = pd.DataFrame({
     # Source: Figure 1 in https://www.taxpolicycenter.org/sites/default/files/alfresco/publication-pdfs/2000587-financial-transaction-taxes.pdf
     'ftt': [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.2, 0.2, 0.2, 0.3, 0.4, 0.8, 1.0]
 })
+
+INCIDENCE[['vat', 'carbon_tax', 'ftt']] /= 100
+
+
+def add_custom_tax(df, segment_income, w, base_income, incidence, prefix):
+    """Add a custom tax based on incidence analysis driven by percentiles.
+
+    Args:
+        df: DataFrame.
+        segment_income: Income measure used to segment tax units into quantiles.
+        w: Weight used to segment into quantiles (either s006 or XTOT_m).
+        base_income: Income measure by which incidence is multiplied to estimate
+            liability.
+        incidence: pandas Series indexed on the floor of an income percentile,
+            with values for the tax rate.
+        prefix: Prefix for columns added.
+
+    Returns:
+        Nothing. Adds two columns to df:
+        * [prefix]_tax_incidence: Rate.
+        * [prefix]_tax_liability: Rate * base_income.
+        df is also sorted by segment_income.
+    """
+    df.sort_values(segment_income, inplace=True)
+    income_percentile = df[w].cumsum() / df[w].sum()
+    df[prefix + '_tax_incidence'] = incidence[
+        pd.cut(income_percentile,
+               bins=incidence.index.tolist() + [100],
+               labels=False)].values
+    df[prefix + '_tax_liability'] = df[prefix + '_tax_incidence'] * df[base_income]
+    
+
+def add_carbon_tax(df, incidence=INCIDENCE):
+    """Estimate carbon tax liability.
+
+    Args:
+        df: DataFrame with tpc_eci and XTOT_m columns. Use TPC methodology to
+            align with their incidence estimate.
+        incidence: DataFrame with income_percentile_floor and carbon_tax columns.
+
+    Returns:
+        Nothing. Two columns are added:
+        * carbon_tax_incidence: Share of income.
+        * carbon_tax_liability: Liability (carbon_tax_incidence * aftertax_income).
+        It also sorts df by tpc_eci as part of the calculation.
+    """
+    df.sort_values('tpc_eci', inplace=True)
+    income_percentile = df.XTOT_m.cumsum() / df.XTOT_m.sum()
+    df['carbon_tax_incidence'] = INCIDENCE.vat[
+        pd.cut(income_percentile,
+               bins=INCIDENCE.income_percentile_floor.tolist() + [100],
+               labels=False)].values
+    df['carbon_tax_liability'] = df.carbon_tax_incidence * df.aftertax_income
--- taxcalc_helpers/utils.py
+++ taxcalc_helpers/utils.py
@@ -237,31 +237,6 @@ def calc_df(records=None,
     return df.set_index('RECID')
 
 
-def cash_income(df):
-    """Calculates income after taxes and cash transfers.
-
-    Defined as aftertax_income minus non-cash benefits.
-
-    Args:
-        df: A Tax-Calculator pandas DataFrame with columns for
-            * aftertax_income
-            * snap_ben
-            * 
-    
-    Returns:
-        A pandas Series with the cash income for each row in df.
-    """
-    return (df.aftertax_income -
-            (1 - tc.HOUSING_CASH_SHARE) * df.housing_ben -
-            (1 - tc.MCAID_CASH_SHARE) * df.mcaid_ben -
-            (1 - tc.MCARE_CASH_SHARE) * df.mcare_ben -
-            (1 - tc.OTHER_CASH_SHARE) * df.other_ben -
-            (1 - tc.SNAP_CASH_SHARE) * df.snap_ben -
-            (1 - tc.SSI_CASH_SHARE) * df.ssi_ben -
-            (1 - tc.TANF_CASH_SHARE) * df.tanf_ben -
-            (1 - tc.VET_CASH_SHARE) * df.vet_ben -
-            (1 - tc.WIC_CASH_SHARE) * df.wic_ben)
-
 
 def weighted_quantile(values, quantiles, sample_weight=None, 
                       values_sorted=False, old_style=False):
