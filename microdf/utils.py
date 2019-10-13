678d699f058f3e82c19a23f0ec12b3be6d959337
Max Ghenis <mghenis@gmail.com>
2019-10-12 22:17:23 -0700

    Create inequality.py and move gini from utils.py.

--- microdf/utils.py
+++ microdf/utils.py
@@ -4,43 +4,6 @@ import taxcalc as tc
 import microdf as mdf
 import collections
 
-def gini(x, w=None, negatives=None):
-    """Calculates Gini index.
-
-    Args:
-        x: A float numpy array of data to calculate Gini index on.
-        w: An optional float numpy array of weights. Should be the same length as x.
-        negatives: An optional string indicating how to treat negative values of x:
-                   'zero' replaces negative values with zeroes.
-                   'shift' subtracts the minimum value from all values of x, 
-                   when this minimum is negative. That is, it adds the absolute minimum value.
-                   Defaults to None, which leaves negative values as they are.
-
-    Returns:
-        A float, the Gini index.
-    """
-    # Requires float numpy arrays (not pandas Series or lists) to work.
-    x = np.array(x).astype('float')
-    if negatives=='zero':
-        x[x < 0] = 0
-    if negatives=='shift' and np.amin(x) < 0:
-        x -= np.amin(x)
-    if w is not None:
-        w = np.array(w).astype('float')
-        sorted_indices = np.argsort(x)
-        sorted_x = x[sorted_indices]
-        sorted_w = w[sorted_indices]
-        cumw = np.cumsum(sorted_w)
-        cumxw = np.cumsum(sorted_x * sorted_w)
-        return (np.sum(cumxw[1:] * cumw[:-1] - cumxw[:-1] * cumw[1:]) /
-                (cumxw[-1] * cumw[-1]))
-    else:
-        sorted_x = np.sort(x)
-        n = len(x)
-        cumxw = np.cumsum(sorted_x)
-        # The above formula, with all weights equal to 1 simplifies to:
-        return (n + 1 - 2 * np.sum(cumxw) / cumxw[-1]) / n
-
 
 def fpl(XTOT):
     """Calculates the federal poverty guideline for a household of a certain size.
