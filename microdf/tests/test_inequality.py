3842572c963e1dbd0f9cc60e879bf48434698293
Max Ghenis <mghenis@gmail.com>
2019-10-12 23:30:02 -0700

    Fix test

--- microdf/tests/test_inequality.py
+++ microdf/tests/test_inequality.py
@@ -2,5 +2,5 @@ import pytest
 import microdf as mdf
 
 def test_top_pct():
-    x = list(range(1, 11))  # 1 to 10. Sum = 10 * 9 / 2 = 45.
-    assert mdf.top_10_pct_share(x) == 10 / 45
+    x = list(range(1, 11))  # 1 to 10. Sum = 10 * 11 / 2 = 55.
+    assert mdf.top_10_pct_share(x) == 10 / 55
