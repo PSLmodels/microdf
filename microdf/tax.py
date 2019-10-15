bf20df2e597276df1713d2e50ed0291e8fe7cf8f
Max Ghenis <mghenis@gmail.com>
2019-10-14 22:11:50 -0700

    Fix PEP8

--- microdf/tax.py
+++ microdf/tax.py
@@ -13,8 +13,8 @@ def tax_from_mtrs(val, brackets, rates):
     #     Series of tax liabilities with the same size as val.
     df_tax = pd.DataFrame({'brackets': brackets, 'rates': rates})
     df_tax['base_tax'] = df_tax.brackets.\
-                         sub(df_tax.brackets.shift(fill_value=0)).\
-                         mul(df_tax.rates.shift(fill_value=0)).cumsum()
+        sub(df_tax.brackets.shift(fill_value=0)).\
+        mul(df_tax.rates.shift(fill_value=0)).cumsum()
     rows = df_tax.brackets.searchsorted(val, side='right') - 1
     income_bracket_df = df_tax.loc[rows].reset_index(drop=True)
     return pd.Series(val).sub(income_bracket_df.brackets).\
