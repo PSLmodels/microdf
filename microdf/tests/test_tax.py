1db3c668bee49718ea3b9045840f390a476cb8eb
Max Ghenis <mghenis@gmail.com>
2019-10-15 10:28:06 -0700

    Add avoidance_rate test.

--- microdf/tests/test_tax.py
+++ microdf/tests/test_tax.py
@@ -11,3 +11,7 @@ def test_tax():
     EXPECTED = [0, 0, 0, 0.1, 1e3]
     res = mdf.tax_from_mtrs(INCOME, BRACKETS, RATES)
     pd.testing.assert_series_equal(res, pd.Series(EXPECTED))
+    # Try with 10% avoidance.
+    EXPECTED_10PCT_AVOIDANCE = [0, 0, 0, 0, 800]
+    res_10pct_avoidance = mdf.tax_from_mtrs(INCOME, BRACKETS, RATES, 0.1)
+    pd.testing.assert_series_equal(res_10pct_avoidance, EXPECTED_10PCT_AVOIDANCE)
