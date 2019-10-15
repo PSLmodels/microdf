249a7db42ea0a32ffb2dffe4357d98ef3b371db2
Max Ghenis <mghenis@gmail.com>
2019-10-15 12:47:43 -0700

    PEP8 fixes

--- microdf/tax.py
+++ microdf/tax.py
@@ -2,7 +2,8 @@ import pandas as pd
 
 
 def mtr(val, brackets, rates):
-    # Calculates the marginal tax rate applied to a value depending on a schedule.
+    # Calculates the marginal tax rate applied to a value depending on a
+    # tax schedule.
     #
     # Args:
     #     val: Value to assess tax on, e.g. wealth or income (list or Series).
@@ -13,11 +14,12 @@ def mtr(val, brackets, rates):
     #     Series of the size of val representing the marginal tax rate.
     df_tax = pd.DataFrame({'brackets': brackets, 'rates': rates})
     df_tax['base_tax'] = df_tax.brackets.\
-                         sub(df_tax.brackets.shift(fill_value=0)).\
-                         mul(df_tax.rates.shift(fill_value=0)).cumsum()
+        sub(df_tax.brackets.shift(fill_value=0)).\
+        mul(df_tax.rates.shift(fill_value=0)).cumsum()
     rows = df_tax.brackets.searchsorted(val, side='right') - 1
     income_bracket_df = df_tax.loc[rows].reset_index(drop=True)
     return income_bracket_df.rates
+
 
 def tax_from_mtrs(val, brackets, rates, avoidance_rate=0,
                   avoidance_elasticity=0):
@@ -30,14 +32,16 @@ def tax_from_mtrs(val, brackets, rates, avoidance_rate=0,
     #     avoidance_rate: Constant avoidance/evasion rate in percentage terms.
     #                     Defaults to zero.
     #     avoidance_elasticity: Avoidance/evasion elasticity.
-    #                           Response of taxable value with respect to tax rate.
+    #                           Response of taxable value with respect
+    #                           to tax rate.
     #                           Defaults to zero. Should be positive.
     #
     # Returns:
     #     Series of tax liabilities with the same size as val.
     assert avoidance_rate == 0 or avoidance_elasticity == 0, \
         "Cannot supply both avoidance_rate and avoidance_elasticity."
-    assert avoidance_elasticity >= 0, "Provide nonnegative avoidance_elasticity."
+    assert avoidance_elasticity >= 0, \
+        "Provide nonnegative avoidance_elasticity."
     df_tax = pd.DataFrame({'brackets': brackets, 'rates': rates})
     df_tax['base_tax'] = df_tax.brackets.\
         sub(df_tax.brackets.shift(fill_value=0)).\
