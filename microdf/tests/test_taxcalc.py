af6c2894de88332f269c75383e084422339a875c
Max Ghenis <mghenis@gmail.com>
2019-12-05 10:07:46 -0800

    Skip taxcalc tests if taxcalc is not installed.

--- microdf/tests/test_taxcalc.py
+++ microdf/tests/test_taxcalc.py
@@ -1,12 +1,21 @@
 import microdf as mdf
 import pytest
-import taxcalc as tc
+
+try:
+    import taxcalc as tc
+    _HAVE_TAXCALC = True
+except ImportError:
+    _HAVE_TAXCALC = False
 
 
 def test_calc_df():
+    if not _HAVE_TAXCALC:
+        pytest.skip("taxcalc is not installed")
     df = mdf.calc_df()
 
 
 def test_static_baseline_calc():
+    if not _HAVE_TAXCALC:
+        pytest.skip("taxcalc is not installed")
     recs = tc.Records.cps_constructor()
     calc = mdf.static_baseline_calc(recs, 2020)
