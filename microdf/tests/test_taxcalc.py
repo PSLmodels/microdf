783be813e9e012077ab3173e2526b198e8c96299
Max Ghenis <mghenis@gmail.com>
2019-12-04 21:39:35 -0800

    Simple tests of taxcalc functions.

---
+++ microdf/tests/test_taxcalc.py
@@ -0,0 +1,12 @@
+import microdf as mdf
+import pytest
+import taxcalc as tc
+
+
+def test_calc_df():
+    df = mdf.calc_df()
+
+
+def test_static_baseline_calc():
+    recs = tc.Records()
+    calc = mdf.static_baseline_calc(recs, 2020)
