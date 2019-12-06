29cc319b1da52683077dec5927c934d4fcda30a7
Max Ghenis <mghenis@gmail.com>
2019-12-05 21:03:53 -0800

    Test kwargs for read_stata_zip

--- microdf/tests/test_io.py
+++ microdf/tests/test_io.py
@@ -4,4 +4,7 @@ import microdf as mdf
 
 def test_read_stata_zip():
     SCF2016 = 'https://www.federalreserve.gov/econres/files/scfp2016s.zip'
-    df = mdf.read_stata_zip(SCF2016)
+    COLS = ['wgt', 'networth']
+    df = mdf.read_stata_zip(SCF2016, columns=COLS)
+    assert df.columns == COLS
+    assert df.shape[0] > 0
