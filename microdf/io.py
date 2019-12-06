8145c73c51644255201605134107a104e324ef56
Max Ghenis <mghenis@gmail.com>
2019-12-05 21:02:05 -0800

    Add kwargs to read_stata_zip

--- microdf/io.py
+++ microdf/io.py
@@ -3,7 +3,7 @@ import pandas as pd
 from urllib.request import urlopen
 import zipfile
 
-def read_stata_zip(url):
+def read_stata_zip(url, **kwargs):
     """Reads zipped Stata file by URL.
 
        From https://stackoverflow.com/a/59122689/1840471
@@ -13,6 +13,7 @@ def read_stata_zip(url):
     Args:
         url: URL string of .zip file containing a single
             .dta file.
+        **kwargs: Arguments passed to pandas.read_stata.
 
     Returns:
         DataFrame.
@@ -21,4 +22,4 @@ def read_stata_zip(url):
         data = io.BytesIO(request.read())
     with zipfile.ZipFile(data) as archive:
         with archive.open(archive.namelist()[0]) as stata:
-            return pd.read_stata(stata)
+            return pd.read_stata(stata, **kwargs)
