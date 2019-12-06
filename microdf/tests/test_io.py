fd9c44389e006ea5e3a23c5d36934b65b9614141
Max Ghenis <mghenis@gmail.com>
2019-12-05 21:05:06 -0800

    Make columns list to assert

--- microdf/tests/test_io.py
+++ microdf/tests/test_io.py
@@ -6,5 +6,5 @@ def test_read_stata_zip():
     SCF2016 = 'https://www.federalreserve.gov/econres/files/scfp2016s.zip'
     COLS = ['wgt', 'networth']
     df = mdf.read_stata_zip(SCF2016, columns=COLS)
-    assert df.columns == COLS
+    assert df.columns.tolist() == COLS
     assert df.shape[0] > 0
