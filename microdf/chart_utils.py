a65306a8da192f607e3b58f1e06d7aa8662e9a65
Max Ghenis <mghenis@gmail.com>
2019-12-13 16:36:10 -0800

    Add optional suffix to dollar_format.

--- microdf/chart_utils.py
+++ microdf/chart_utils.py
@@ -1,12 +1,14 @@
 import matplotlib as mpl
 
-def dollar_format():
+
+def dollar_format(suffix=''):
     """ Dollar formatter for matplotlib.
 
     Args:
-        None.
+        suffix: Suffix to append, e.g. 'B'. Defaults to ''.
 
     Returns:
         FuncFormatter.
     """
-    return mpl.ticker.FuncFormatter(lambda x, _: '$' + format(int(x), ','))
+    return mpl.ticker.FuncFormatter(
+        lambda x, _: '$' + format(int(x), ',') + suffix)
