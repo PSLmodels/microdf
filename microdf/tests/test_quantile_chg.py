a34832979d58e909a62be2f41172981469e615af
Max Ghenis <mghenis@gmail.com>
2020-04-11 22:43:08 -0700

    Test quantile_chg and quantile_chg_plot

---
+++ microdf/tests/test_quantile_chg.py
@@ -0,0 +1,10 @@
+import microdf as mdf
+
+def test_quantile_chg():
+    mdf.quantile_chg(v1=[1, 2, 3], v2=[4, 5, 6],
+                     w1=[7, 8, 9], w2=[10, 11, 12])
+
+
+def test_quantile_chg_plot():
+    mdf.quantile_chg_plot(v1=[1, 2, 3], v2=[4, 5, 6],
+                          w1=[7, 8, 9], w2=[10, 11, 12])
