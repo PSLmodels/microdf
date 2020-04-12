d14a42bfe7a8cbbece8e0565be86fe728e51720f
Max Ghenis <mghenis@gmail.com>
2020-04-12 09:27:43 -0700

    Use shades of a single color. Fixes #81.

--- microdf/charts.py
+++ microdf/charts.py
@@ -40,6 +40,8 @@ def quantile_chg_plot(v1, v2, w1=None, w2=None, q=np.arange(0.1, 1, 0.1),
     ax.axhline(0, color='lightgray', zorder=-1)
     # Looks better narrower.
     plt.gcf().set_size_inches(4, 6)
+    # Make shades of green, removing the lightest 10 shades.
+    sns.set_palette(sns.color_palette('Greens', q.size + 11)[11:])
     return ax
 
 
