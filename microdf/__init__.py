fdf9d2b773d818d6bd6f27a724f2a527cc8acfe4
Max Ghenis <mghenis@gmail.com>
2020-08-25 14:21:47 -0700

    Add __all__ to __init__.py

--- microdf/__init__.py
+++ microdf/__init__.py
@@ -15,3 +15,64 @@ from .utils import *
 from .weighted import *
 
 name = "microdf"
+
+__all__ = [
+    # agg.py
+    "combine_base_reform",
+    "pctchg_base_reform",
+    "agg",
+    # chart_utils.py
+    "dollar_format",
+    # charts.py
+    "quantile_chg_plot",
+    "quantile_pct_chg_plot",
+    # custom_taxes.py
+    "add_custom_tax",
+    "add_vat",
+    "add_carbon_tax",
+    "add_ftt",
+    # income_measures.py
+    "cash_income",
+    "tpc_eci",
+    "market_income",
+    # inequality.py
+    "gini",
+    "top_x_pct_share",
+    "bottom_x_pct_share",
+    "bottom_50_pct_share",
+    "top_10_pct_share",
+    "top_1_pct_share",
+    "top_0_1_pct_share",
+    "t10_b50",
+    # io.py
+    "read_stata_zip",
+    # poverty.py
+    "fpl",
+    # style.py
+    "set_plot_style",
+    # tax.py
+    "mtr",
+    "tax_from_mtrs",
+    # taxcalc.py
+    "static_baseline_calc",
+    "add_weighted_metrics",
+    "n65",
+    "calc_df",
+    "recalculate",
+    # ubi.py
+    "ubi_or_bens",
+    # utils.py
+    "ordinal_label",
+    "dedup_list",
+    "listify",
+    "flatten",
+    "cartesian_product",
+    # weighted.py
+    "weight",
+    "weighted_sum",
+    "weighted_mean",
+    "weighted_quantile",
+    "weighted_median",
+    "add_weighted_quantiles",
+    "quantile_chg",
+]
