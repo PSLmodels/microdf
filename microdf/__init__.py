e043c035f7e53d9b0cf9caa24a2da32a2d85f02a
Max Ghenis <mghenis@gmail.com>
2020-08-25 15:03:37 -0700

    List each imported item separately in __init__.py

--- microdf/__init__.py
+++ microdf/__init__.py
@@ -1,18 +1,65 @@
-from .agg import *
-from .chart_utils import *
-from .charts import *
-from .constants import *
-from .custom_taxes import *
-from .income_measures import *
-from .inequality import *
-from .io import *
-from .poverty import *
-from .style import *
-from .tax import *
-from .taxcalc import *
-from .ubi import *
-from .utils import *
-from .weighted import *
+from .agg import (
+    combine_base_reform,
+    pctchg_base_reform,
+    agg
+)
+from .chart_utils import dollar_format
+from .charts import (
+    quantile_chg_plot,
+    quantile_pct_chg_plot
+_
+from .custom_taxes import (
+    add_custom_tax,
+    add_vat,
+    add_carbon_tax,
+    add_ftt
+)
+from .income_measures import (
+    cash_income,
+    tpc_eci,
+    market_income
+)
+from .inequality import (
+    gini,
+    top_x_pct_share,
+    bottom_x_pct_share,
+    bottom_50_pct_share,
+    top_10_pct_share,
+    top_1_pct_share,
+    top_0_1_pct_share,
+    t10_b50
+)
+from .io import read_stata_zip
+from .poverty import fpl
+from .style import set_plot_style
+from .tax import (
+    mtr,
+    tax_from_mtrs
+)
+from .taxcalc import (
+    static_baseline_calc,
+    add_weighted_metrics,
+    n65,
+    calc_df,
+    recalculate
+)
+from .ubi import ubi_or_bens
+from .utils import (
+    ordinal_label,
+    dedup_list,
+    listify,
+    flatten,
+    cartesian_product
+)
+from .weighted import (
+    weight,
+    weighted_sum,
+    weighted_mean,
+    weighted_quantile,
+    weighted_median,
+    add_weighted_quantiles,
+    quantile_chg
+)
 
 name = "microdf"
 __version__ = "0.1.0"
