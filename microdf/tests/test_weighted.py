6f59c3bf7bab4140569ae1212e00f328a45ef3ec
Max Ghenis <mghenis@gmail.com>
2020-12-07 23:07:46 -0800

    Add groupby to weighted_{sum,mean,median} functions.

--- microdf/tests/test_weighted.py
+++ microdf/tests/test_weighted.py
@@ -6,6 +6,11 @@ import microdf as mdf
 X = [1, 5, 2]
 W = [4, 1, 1]
 df = pd.DataFrame({"x": X, "w": W})
+# Also make a version with groups.
+df2 = df.copy(deep=True)
+df2.x *= 2
+dfg = pd.concat([df, df2])
+dfg["g"] = ["a"] * 3 + ["b"] * 3
 
 
 def test_weighted_quantile():
@@ -14,15 +19,19 @@ def test_weighted_quantile():
 
 
 def test_weighted_median():
-    assert mdf.weighted_median(df, 'x') == 2
+    assert mdf.weighted_median(df, "x") == 2
     mdf.weighted_median(df, "x", "w")
+    # Test with groups.
+    mdf.weighted_median(dfg, "x", "w", "g")
 
 
 def test_weighted_mean():
-    assert mdf.weighted_mean(df, 'x') == 8 / 3
+    assert mdf.weighted_mean(df, "x") == 8 / 3
     assert mdf.weighted_mean(df, "x", "w") == 11 / 6
+    mdf.weighted_mean(dfg, "x", "w", "g")
 
 
 def test_weighted_sum():
-    assert mdf.weighted_sum(df, 'x') == 8
+    assert mdf.weighted_sum(df, "x") == 8
     assert mdf.weighted_sum(df, "x", "w") == 11
+    mdf.weighted_sum(dfg, "x", "w", "g")
