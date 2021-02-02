9cbd0ba47bb02a20cedb83ea07b19a74882f662d
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2021-02-02 00:15:31 +0000

    Add tests for MicroSeries

---
+++ microdf/tests/test_generic.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+import microdf as mdf
+
+def test_sum():
+    arr = np.array([0, 1, 1])
+    w = np.array([3, 0, 9])
+    series = mdf.MicroSeries(arr, weights=w)
+    assert series.sum() == (arr * w).sum()
+
+    arr = np.linspace(-20, 100, 100)
+    w = np.linspace(1, 3, 100)
+    series = mdf.MicroSeries(arr)
+    series.set_weights(w)
+    assert series.sum() == (arr * w).sum()
+
+    w = np.linspace(1, 3, 101)
+    series = mdf.MicroSeries(arr)
+    try:
+        series.set_weights(w)
+        assert False
+    except:
+        pass
+
+def test_mean():
+    arr = np.array([3, 0, 2])
+    w = np.array([4, 1, 1])
+    series = mdf.MicroSeries(arr, weights=w)
+    assert series.mean() == np.average(arr, weights=w)
+
+    arr = np.linspace(-20, 100, 100)
+    w = np.linspace(1, 3, 100)
+    series = mdf.MicroSeries(arr)
+    series.set_weights(w)
+    assert series.mean() == np.average(arr, weights=w)
+
+    w = np.linspace(1, 3, 101)
+    series = mdf.MicroSeries(arr)
+    try:
+        series.set_weights(w)
+        assert False
+    except:
+        pass
