be057e76b0856e5af983075e42b7c3f5ec6d27e7
Max Ghenis <mghenis@gmail.com>
2021-02-07 09:44:03 -0800

    Add remaining inequality functions and docstrings.

--- microdf/generic.py
+++ microdf/generic.py
@@ -85,7 +85,20 @@ class MicroSeries(pd.Series):
         """
         return self.quantile(0.5)
 
-    def gini(self, negatives=None):
+    def gini(self, negatives: str=None) -> float:
+        """Calculates Gini index.
+
+        :param negatives: An optional string indicating how to treat negative
+            values of x:
+            'zero' replaces negative values with zeroes.
+            'shift' subtracts the minimum value from all values of x,
+            when this minimum is negative. That is, it adds the absolute
+            minimum value.
+            Defaults to None, which leaves negative values as they are.
+        :type q: str
+        :returns: Gini index.
+        :rtype: float
+        """
         x = np.array(self).astype("float")
         if negatives == "zero":
             x[x < 0] = 0
@@ -107,7 +120,79 @@ class MicroSeries(pd.Series):
             # The above formula, with all weights equal to 1 simplifies to:
             return (n + 1 - 2 * np.sum(cumxw) / cumxw[-1]) / n
 
-    
+    def top_x_pct_share(self, top_x_pct: float) -> float:
+        """Calculates top x% share.
+
+        :param top_x_pct: Decimal between 0 and 1 of the top %, e.g. 0.1, 0.001.
+        :type top_x_pct: float
+        :returns: The weighted share held by the top x%.
+        :rtype: float
+        """
+        threshold = self.quantile(1 - top_x_pct)
+        top_x_pct_sum = self[self >= threshold].sum()
+        total_sum = self.sum()
+        return top_x_pct_sum / total_sum
+
+    def bottom_x_pct_share(self, bottom_x_pct) -> float:
+        """Calculates bottom x% share.
+
+        :param bottom_x_pct: Decimal between 0 and 1 of the top %, e.g. 0.1, 0.001.
+        :type bottom_x_pct: float
+        :returns: The weighted share held by the bottom x%.
+        :rtype: float
+        """
+        return 1 - self.top_x_pct_share(1 - bottom_x_pct)
+
+    def bottom_50_pct_share(self) -> float:
+        """Calculates bottom 50% share.
+
+        :returns: The weighted share held by the bottom 50%.
+        :rtype: float
+        """
+        return self.bottom_x_pct_share(0.5)
+
+    def top_50_pct_share(self) -> float:
+        """Calculates top 50% share.
+
+        :returns: The weighted share held by the top 50%.
+        :rtype: float
+        """
+        return self.top_x_pct_share(0.5)
+
+    def top_10_pct_share(self) -> float:
+        """Calculates top 10% share.
+
+        :returns: The weighted share held by the top 10%.
+        :rtype: float
+        """
+        return self.top_x_pct_share(0.1)
+
+    def top_1_pct_share(self) -> float:
+        """Calculates top 1% share.
+
+        :returns: The weighted share held by the top 50%.
+        :rtype: float
+        """
+        return self.top_x_pct_share(0.01)
+
+    def top_0_1_pct_share(self) -> float:
+        """Calculates top 0.1% share.
+
+        :returns: The weighted share held by the top 0.1%.
+        :rtype: float
+        """
+        return self.top_x_pct_share(0.001)
+
+    def t10_b50(self):
+        """Calculates ratio between the top 10% and bottom 50% shares.
+
+        :returns: The weighted share held by the top 10% divided by
+            the weighted share held by the bottom 50%.
+
+        """
+        t10 = self.top_10_pct_share()
+        b50 = self.bottom_50_pct_share()
+        return t10 / b50
 
 
 class MicroDataFrame(pd.DataFrame):
@@ -159,14 +244,55 @@ class MicroDataFrame(pd.DataFrame):
         self.weight_col = column
         self._link_all_weights()
 
-    def poverty_rate(self, income: str, threshold: str):
+    def poverty_rate(self, income: str, threshold: str) -> float:
+        """Calculate poverty rate, i.e., the population share with income
+        below their poverty threshold.
+
+        :param income: Column indicating income.
+        :type income: str
+        :param threshold: Column indicating threshold.
+        :type threshold: str
+        :return: Poverty rate between zero and one.
+        :rtype: float
+        """
         return mdf.poverty_rate(self, income, threshold, self.weights)
 
-    def deep_poverty_rate(self, income: str, threshold: str):
+    def deep_poverty_rate(self, income: str, threshold: str) -> float:
+        """Calculate deep poverty rate, i.e., the population share with income
+        below half their poverty threshold.
+
+        :param income: Column indicating income.
+        :type income: str
+        :param threshold: Column indicating threshold.
+        :type threshold: str
+        :return: Deep poverty rate between zero and one.
+        :rtype: float
+        """
         return mdf.deep_poverty_rate(self, income, threshold, self.weights)
 
-    def poverty_gap(self, income: str, threshold: str):
+    def poverty_gap(self, income: str, threshold: str) -> float:
+        """Calculate poverty gap, i.e., the total gap between income and
+        poverty thresholds for all people in poverty.
+
+        :param income: Column indicating income.
+        :type income: str
+        :param threshold: Column indicating threshold.
+        :type threshold: str
+        :return: Poverty gap.
+        :rtype: float
+        """
         return mdf.poverty_gap(self, income, threshold, self.weights)
 
-    def squared_poverty_gap(self, income: str, threshold: str):
+    def squared_poverty_gap(self, income: str, threshold: str) -> float:
+        """Calculate squared poverty gap, i.e., the total squared gap between
+        income and poverty thresholds for all people in poverty.
+        Also known as the poverty severity index.
+
+        :param income: Column indicating income.
+        :type income: str
+        :param threshold: Column indicating threshold.
+        :type threshold: str
+        :return: Squared poverty gap.
+        :rtype: float
+        """
         return mdf.poverty_gap(self, income, threshold, self.weights)
