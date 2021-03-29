07438b77c012ceae6ca7f48b0b739f5e09c3c58d
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2021-03-29 12:32:22 +0100

    Add weights to print output

--- microdf/generic.py
+++ microdf/generic.py
@@ -373,6 +373,11 @@ class MicroSeries(pd.Series):
     def __pos__(self, other):
         return MicroSeries(super().__pos__(other), weights=self.weights)
 
+    def __repr__(self):
+        return pd.DataFrame(
+            dict(value=self.values, weight=self.weights.values)
+        ).__repr__()
+
 
 MicroSeries.SCALAR_FUNCTIONS = [
     fn
@@ -755,3 +760,8 @@ class MicroDataFrame(pd.DataFrame):
         """
         in_poverty = income < threshold
         return in_poverty.sum()
+
+    def __repr__(self):
+        df = pd.DataFrame(self)
+        df["weight"] = self.weights
+        return df[[df.columns[-1]] + list(df.columns[:-1])].__repr__()
