391c51c110b02e54ae7da7380646d5836fdbeaf9
Max Ghenis <mghenis@gmail.com>
2021-07-01 08:59:09 -0700

    Remove quantile_chg_plot to drop matplotlib-label-lines dep

--- microdf/charts.py
+++ microdf/charts.py
@@ -1,67 +1,9 @@
-import labellines as ll
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
 import microdf as mdf
-
-
-def quantile_chg_plot(
-    df1,
-    df2,
-    col1,
-    col2,
-    w1=None,
-    w2=None,
-    q=None,
-    label1="Base",
-    label2="Reform",
-    title="Change in disposable income percentiles",
-    currency="USD",
-):
-    """Create plot with one line per quantile boundary between base and
-        reform.
-
-    :param df1: DataFrame with first set of values.
-    :param df2: DataFrame with second set of values.
-    :param col1: Name of columns with values in df1.
-    :param col2: Name of columns with values in df2.
-    :param w1: Name of weight column in df1.
-    :param w2: Name of weight column in df2.
-    :param q: Quantiles. Defaults to decile boundaries.
-    :param label1: Label for left side of x-axis. Defaults to 'Base'.
-    :param label2: Label for right side of x-axis. Defaults to 'Reform'.
-    :returns: Axis.
-
-    """
-    # Calculate the q default because it's later used for defining a color
-    # palette.
-    if q is None:
-        q = np.arange(0.1, 1, 0.1)
-    # Calculate weighted quantiles.
-    df = mdf.quantile_chg(df1, df2, col1, col2, w1, w2, q)
-    # Make shades of green, removing the lightest 10 shades.
-    with sns.color_palette(sns.color_palette("Greens", q.size + 11)[11:]):
-        ax = df.plot()
-    # Label the start and end points.
-    plt.xticks([0, 1], [label1, label2])
-    # Label the lines instead of using a legend.
-    ax.get_legend().remove()
-    # Move line labels closer to the center.
-    ll.labelLines(plt.gca().get_lines(), xvals=(0.1, 0.9))
-    # Formatting.
-    plt.title(title)
-    plt.ylim(0, None)
-    ax.grid(color=mdf.GRID_COLOR, axis="y")
-    formatter = mdf.currency_format(currency)
-    ax.yaxis.set_major_formatter(formatter)
-    ax.yaxis.set_minor_formatter(formatter)
-    sns.despine(left=True, bottom=True)
-    ax.axhline(0, color="lightgray", zorder=-1)
-    # Looks better narrower.
-    plt.gcf().set_size_inches(4, 6)
-    return ax
 
 
 def quantile_pct_chg_plot(df1, df2, col1, col2, w1=None, w2=None, q=None):
